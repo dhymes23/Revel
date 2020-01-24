994a302c8e6afe91ebe61520a9f2fa6449f3734d
dhymes23 <58163885+dhymes23@users.noreply.github.com>
2020-01-24 13:37:02 -0800

    Update UserLogin.js

--- App/screens/UserLogin.js
+++ App/screens/UserLogin.js
@@ -1,53 +1,60 @@
-// Screen for the user login
-import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  StatusBar
-} from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View, Button,TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View>
+      <Text style ={{color :'#3bde26', fontWeight: 'bold', textAlign:'center',fontSize:60,paddingBottom: 30, fontFamily:'HelveticaNeue-Italic'}}>Revel</Text>
+      <Text style = {{color:'#3bde26',fontFamily:'HelveticaNeue-Italic'}}>Username:</Text>
+      <View style = {{backgroundColor:'#3bde26',width:230}}>
+      <TextInput style = {{borderBottomColor:"#000",height:30}}/>
+      </View>
+       <Text style = {{color:'#3bde26',fontFamily:'HelveticaNeue-Italic'}}>Password:</Text>
+       <View style = {{backgroundColor:'#3bde26',width:230}}>
+       <TextInput style = {{borderBottomColor:"#000",height:30}}/> 
+       </View>
+       <View>
+      <Button title = "Log-in" style = {styles.buttonStyle}/>
+      <Button title ="Sign-Up" style ={styles.buttonStyle}/>
+      </View>
+      </View>
+    </View>
+  );
+}
+
+function onPressLogIn(){
+  
+}
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
     flex: 1,
-    paddingHorizontal: 20,
-    alignItems: "center"
+    backgroundColor: '#fff',
+    padding: 30,
   },
-  text: {
-    color: "#303030",
-    fontSize: 30,
-    textAlign: "center",
-    letterSpacing: -0.02,
-    fontWeight: "600"
+  buttonStyle:{
+    color:'#000',
+    borderColor:'#000',
+    textAlign: 'center',
   },
-  button: {
-    backgroundColor: "#3bde26",
-    borderColor: "#303030",
-    borderLeftColor: "#303030",
-    marginTop: 30,
-    alignItems: "center",
-    justifyContent: "center",
-    width: 250,
-    height: 50,
-    textAlign: "center"
+  textInputStyle:{
+    borderRadius:40,
+    borderColor:'#000000',
+    padding:20,
+    height:30
+    
+ 
   },
-  buttonText: {
-    color: "#303030",
-    fontSize: 30,
-    fontWeight: "700"
+  topText:{
+    alignContent: 'center',
+    justifyContent: 'space-around',
+    textAlign: 'center',
+    
+  },
+  middleText:{
+    padding: 350,
+    justifyContent: 'center',
   }
-});
 
-export default ({ navigation }) => (
-  <View style={styles.container}>
-    <StatusBar barStyle="light-content" />
-    <TouchableOpacity
-      onPress={() => navigation.navigate("MainMenu")}
-      style={styles.button}
-    >
-      <Text style={styles.buttonText}>Back</Text>
-    </TouchableOpacity>
-  </View>
-);
+});
