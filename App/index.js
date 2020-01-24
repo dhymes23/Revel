cae2a91989e2b59d2d9d48d4549dede709aed744
gmulder15 <59668598+gmulder15@users.noreply.github.com>
2020-01-24 13:32:13 -0800

    Update index.js

--- App/index.js
+++ App/index.js
@@ -1,19 +1,68 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Revel</Text>
-    </View>
-  );
-}
+import MainMenu from "./screens/MainMenu";
+import FoodInfo from "./screens/FoodInfo";
+import ASP from "./screens/ASP";
+import CommunityPost from "./screens/CommunityPost";
+import CommunitySearch from "./screens/CommunitySearch";
+import CreateAccount from "./screens/CreateAccount";
+import ScanItem from "./screens/ScanItem";
+import UserLogin from "./screens/UserLogin";
+import UserProfile from "./screens/UserProfile";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+const MainStack = createStackNavigator({
+  MainMenu: {
+    screen: MainMenu
+  },
+  ASP: {
+    screen: ASP,
+    navigationOptions: {
+      headerTitle: "ASP"
+    }
+  },
+  FoodInfo: {
+    screen: FoodInfo,
+    navigationOptions: {
+      headerTitle: "FoodInfo"
+    }
+  },
+  CommunityPost: {
+    screen: CommunityPost,
+    navigationOptions: {
+      headerTitle: "CommunityPost"
+    }
+  },
+  CommunitySearch: {
+    screen: CommunitySearch,
+    navigationOptions: {
+      headerTitle: "CommunitySearch"
+    }
+  },
+  CreateAccount: {
+    screen: CreateAccount,
+    navigationOptions: {
+      headerTitle: "CreateAccount"
+    }
+  },
+  ScanItem: {
+    screen: ScanItem,
+    navigationOptions: {
+      headerTitle: "ScanItem"
+    }
+  },
+  UserLogin: {
+    screen: UserLogin,
+    navigationOptions: {
+      headerTitle: "UserLogin"
+    }
+  },
+  UserProfile: {
+    screen: UserProfile,
+    navigationOptions: {
+      headerTitle: "UserProfile"
+    }
   }
 });
+
+export default createAppContainer(MainStack);
