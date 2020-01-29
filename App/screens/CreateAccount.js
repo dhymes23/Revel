8c1fc0d61bbd9dc4403911e39d924441f1d2f182
gmulder15 <59668598+gmulder15@users.noreply.github.com>
2020-01-28 20:10:28 -0800

    Update CreateAccount.js

--- App/screens/CreateAccount.js
+++ App/screens/CreateAccount.js
@@ -8,6 +8,20 @@ import {
   StatusBar
 } from "react-native";
 
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Create Username:</Text>
+      <TextInput/>
+      <Text>Enter Passwaord:</Text>
+      <TextInput/>
+      <Text>Re-Enter Password</Text>
+      <TextInput/>
+      <Button>Submit:</Button>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
