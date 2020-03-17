9d3963b4c1ed425bbce6da5ce3b65a03c8a53329
dhymes23 <58163885+dhymes23@users.noreply.github.com>
2020-03-17 11:51:06 -0700

    Update FoodInfo.js

--- App/screens/FoodInfo.js
+++ App/screens/FoodInfo.js
@@ -10,6 +10,17 @@ import {
   Image
 } from "react-native";
 
+const container2=styled.View`
+    flex:1;
+    border:1px solid #ccc;
+    margin:2px 0;
+    border-radius:10px;
+    box-shadow:0 0 10px #ccc;
+    background-color:#fff;
+    width:80%;
+    padding:10px;
+    `
+
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
@@ -25,32 +36,46 @@ const styles = StyleSheet.create({
     fontWeight: "600"
   },
   button: {
-    backgroundColor: "#3bde26",
-    borderColor: "#303030",
-    borderLeftColor: "#303030",
-    marginTop: 30,
-    alignItems: "center",
-    justifyContent: "center",
-    width: 250,
-    height: 50,
-    textAlign: "center"
+    height: 60,
+    width:160,
+    borderRadius:10,  
+    backgroundColor : '#3bde26',
+    marginRight:15,
+    marginTop:100,
+    alignItems:"center",
   },
   buttonText: {
     color: "#303030",
     fontSize: 30,
     fontWeight: "700",
     textAlign: "center"
-  }
-});
+  },
+  
 
-export default ({ navigation }) => (
+});
+var prodName;
+
+
+class FoodInfo extends React.Component{
+
+
+
+  render(){
+
+  return(
   <View style={styles.container}>
     <StatusBar barStyle="light-content" />
-    <Image source={require("./image/Key.png")} />
-    <Text style={styles.buttonText}>
-      Here are the ingredients of the scanned item:
+    <Text style = {{fontWeight:"bold",fontSize:35,padding:20,fontFamily:'HelveticaNeue-Italic',color:'#3bde26', alignSelf:"center"}}>
+     Food Info 
     </Text>
-    <Image source={require("./image/test.png")} />
+    <View style ={{alignContent:'stretch'}}>
+      <Text style = {{fontWeight:'bold',fontSize:20,padding:25}}>Product:</Text>
+    </View>
+    <container2>
+      <Text>Ingredients & Info:</Text>
+
+    
+    </container2>
     <TouchableOpacity
       onPress={() => navigation.navigate("MainMenu")}
       style={styles.button}
@@ -58,4 +83,7 @@ export default ({ navigation }) => (
       <Text style={styles.buttonText}>MainMenu</Text>
     </TouchableOpacity>
   </View>
-);
+  );
+}
+}
+export default FoodInfo
