19cf706df2e88efc669c7b7cf44e0b323ce00ef3
gmulder15 <59668598+gmulder15@users.noreply.github.com>
2020-03-31 15:10:47 -0700

    Update CommunityPost.js

--- App/screens/CommunityPost.js
+++ App/screens/CommunityPost.js
@@ -1,11 +1,14 @@
 // Screen for posting to the community page
+var product;
+var post;
 import React from "react";
 import {
   View,
   Text,
   StyleSheet,
   TouchableOpacity,
-  StatusBar
+  StatusBar,
+  TextInput
 } from "react-native";
 
 const styles = StyleSheet.create({
@@ -37,17 +40,71 @@ const styles = StyleSheet.create({
     color: "#303030",
     fontSize: 30,
     fontWeight: "700"
+  },
+  container1: {
+    flex: 1,
+    justifyContent: "center",
+    paddingTop: 30,
+    backgroundColor: "#3bde26",
+    padding: 8
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center"
   }
 });
 
 export default ({ navigation }) => (
   <View style={styles.container}>
-    <StatusBar barStyle="light-content" />
-    <TouchableOpacity
-      onPress={() => navigation.navigate("MainMenu")}
-      style={styles.button}
-    >
-      <Text style={styles.buttonText}>Back</Text>
-    </TouchableOpacity>
+    <View>
+      <Text
+        style={{
+          fontWeight: "bold",
+          fontSize: 35,
+          padding: 20,
+          fontFamily: "HelveticaNeue-Italic",
+          color: "#3bde26"
+        }}
+      >
+        Post About Product
+      </Text>
+      <Text
+        style={{
+          fontWeight: "bold",
+          fontSize: 35,
+          padding: 20,
+          fontFamily: "HelveticaNeue-Italic",
+          color: "#3bde26"
+        }}
+      ></Text>
+      <Text style={{ fontWeight: "bold", fontSize: 20 }}>Product:</Text>
+      <View style={{ backgroundColor: "#3bde26", height: 50 }}>
+        <TextInput style={{ fontSize: 30 }} value={product} />
+      </View>
+      <Text style={{ fontWeight: "bold", fontSize: 20 }}>Message:</Text>
+      <View style={{ backgroundColor: "#3bde26", height: 100 }}>
+        <TextInput style={{ fontSize: 30 }} multiLint={true} value={post} />
+      </View>
+    </View>
+    <View>
+      <StatusBar barStyle="light-content" />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => alert("Post Created!")}
+      >
+        <Text style={styles.buttonText}>Post</Text>
+      </TouchableOpacity>
+    </View>
+    <View>
+      <StatusBar barStyle="light-content" />
+      <TouchableOpacity
+        onPress={() => navigation.navigate("MainMenu")}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Back</Text>
+      </TouchableOpacity>
+    </View>
   </View>
 );
