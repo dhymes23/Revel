ba3e3bbf6f4dae6e362c72340ceb481e2ffcd334
dhymes23 <58163885+dhymes23@users.noreply.github.com>
2020-04-11 16:53:12 -0700

    Update CreateAccount.js

--- App/screens/CreateAccount.js
+++ App/screens/CreateAccount.js
@@ -1,10 +1,6 @@
 // Screen for creating an account
 
-var psswrd;
-var psswrd2;
-var email;
-var usrNme;
-checkCredentials = accountAuthentify(usrNme,psswrd,psswrd2,email);
+
 import React from "react";
 import {
   View,
@@ -14,6 +10,8 @@ import {
   StatusBar,
   TextInput,
 } from "react-native";
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -46,65 +44,47 @@ const styles = StyleSheet.create({
     fontWeight: "700"
   }
 });
-function accountAuthentify(userName, p1,p2, email){
-  // userNameContiansnum = false;
-  // userNamecharCount = false; 
-  // psswrdMatch = false;
-  // Checks to see if user input matches authenticity requirements 
-  var userName = 'Stupid';
-  var p1;
-  var p2;
-  var email;
-  var numCount = 0;
-  var authenticEntry = true;
-  if(userName.length < 6){
-    authenticEntry = false;
-  for(i = 0; i <= userName.length; i++ ){
-      if(typeof userName.charAt(i) == "number"){
-        numCount++;
-        if(numCount == 2){
-          break;
-        }else{
-          authenticEntry = false;
-        }
-      }
-    }
-  }
-    numCount = 0;
-    // Password Authentification 
-    if(p1 != p2){
-      authenticEntry = false;
-      if(p1.length < 8){
-        authenticEntry = false;
-        for(i = 0; i <= userName.length; i++ ){
-          if(typeof userName.charAt(i) == "number"){
-            numCount++;
-            if(numCount == 2){
-              break;
-            }else{
-              authenticEntry = false;
-            }
-          }
-        }
-      }
-      
-    }
-    
-    // Write Code to check is user name is in the database already
-    // Write Code to check if email is in the Database
-    
-    if(authenticEntry = true && userName != 'undefined'){
-      return userName,p1,p2,email;
-      // navigation.navigate("MainMenu");
-    }
-    else{
-          alert('Username must cotain 8 charaters');
-    }
+
+class CreateAccount extends React.Component{
+
+state = {
+  userName: '',
+  Password: '',
+  Password2: '',
+  email: '',
+  name: '',
+};
+
+handleUserName = (text) => {
+  this.setState({userName:text})
+}
+handlePass = (text) => {
+  this.setState({Password:text})
+}
+handlePassCheck = (text) => {
+  this.setState({Password2:text})
+}
+handleEmail = (text) => {
+  this.setState({email:text})
+}
+handleName = (text) => {
+  this.setState({name:text})
+}
+
+logInfo = (userName,Password,Password2,Email) => {
+ 
+ var uCheck = true;
+ var pcheck = true;
+
+ 
+
+ 
+
   
-  }
-
-
-export default ({ navigation }) => (
+}
+ 
+  render(){
+    return(
   <View style={styles.container}>
     <View>
     <Text style = {{fontWeight:"bold",fontSize:35,padding:20,fontFamily:'HelveticaNeue-Italic',color:'#3bde26'}}>Create Account</Text>
@@ -114,25 +94,29 @@ export default ({ navigation }) => (
     </View>
     <Text style = {{fontWeight:"bold",fontSize:20}}>E-mail:</Text>
     <View style = {{backgroundColor:'#3bde26',height:35}}>
-    <TextInput value = {email}/>
+    <TextInput onChangeText={this.handleEmail}
+                />
     </View>
     <Text style = {{fontWeight:"bold",fontSize:20}}>Username:(mimimum 6 characters)</Text>
     <View style = {{backgroundColor:'#3bde26',height:35}}>
-    <TextInput value = {usrNme}/>
+    <TextInput onChangeText={this.handleUserName}>
+    </TextInput>
     </View>
     <Text style = {{fontWeight:"bold",fontSize:20}}>Password:</Text>
     <View style = {{backgroundColor:'#3bde26',height:35}}>
-    <TextInput value = {psswrd}/>
+    <TextInput onChangeText = {this.handlePass}/>
     </View>
     <Text style = {{fontWeight:"bold",fontSize:20}}>Re-Enter Password:</Text>
     <View style = {{backgroundColor:'#3bde26',height:35,paddingBottom: 20}}>
-    <TextInput value = {psswrd2}/>
+    <TextInput onChangeText ={this.handlePassCheck}/>
     </View>
     </View>
     <View>
     <StatusBar barStyle="light-content" />
     <TouchableOpacity style={styles.button} 
-      onPress= {() => checkCredentials}
+       onPress= {() => this.logInfo(this.state.userName,this.state.Password,this.state.Password2,this.state.email)
+      }
+        // accountAuthentify(this.state.userName,this.state.Password,this.state.Password2,this.state.email)}
       >
       <Text style={styles.buttonText}>Submit</Text>
     </TouchableOpacity>
@@ -148,8 +132,62 @@ export default ({ navigation }) => (
     </TouchableOpacity>
     </View>
   </View>
-);
+    );
+  }
+}
 
-
+function accountAuthentify(userName, p1,p2, email){
+    // userNameContiansnum = false;
+    // userNamecharCount = false; 
+    // psswrdMatch = false;
+    // Checks to see if user input matches authenticity requirements 
+    
+    var numCount = 0;
+    var authenticEntry = true;
+    if(userName.length < 6){
+      authenticEntry = false;
+    for(i = 0; i <= userName.length; i++ ){
+        if(typeof userName.charAt(i) == "number"){
+          numCount++;
+          if(numCount == 2){
+            break;
+          }else{
+            authenticEntry = false;
+          }
+        }
+      }
+    }
+      numCount = 0;
+      // Password Authentification 
+      if(p1 != p2){
+        authenticEntry = false;
+        if(p1.length < 8){
+          authenticEntry = false;
+          for(i = 0; i <= userName.length; i++ ){
+            if(typeof userName.charAt(i) == "number"){
+              numCount++;
+              if(numCount == 2){
+                break;
+              }else{
+                authenticEntry = false;
+              }
+            }
+          }
+        }
+        
+      }
+      
+      // Write Code to check is user name is in the database already
+      // Write Code to check if email is in the Database
+      
+      if(authenticEntry = true && userName != 'undefined'){
+        return userName,p1,p2,email;
+        // navigation.navigate("MainMenu");
+      }
+      else{
+            alert('Must change the following');
+      }
+    
+    }
   
-
+export default CreateAccount;
