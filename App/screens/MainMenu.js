d1117e918a06e188dd254f8714548d0577c6dc7b
gmulder15 <59668598+gmulder15@users.noreply.github.com>
2020-04-13 21:06:22 -0700

    Update MainMenu.js

--- App/screens/MainMenu.js
+++ App/screens/MainMenu.js
@@ -9,17 +9,18 @@ import {
   StatusBar,
   ScrollView
 } from "react-native";
+import { Button, Paragraph, Menu, Provider } from "react-native-paper";
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: "#fff",
     flex: 1,
     alignItems: "center",
-    alignSelf: 'stretch',
+    alignSelf: "stretch"
   },
   scrollview: {
     backgroundColor: "#fff",
-    alignSelf: 'stretch',
+    alignSelf: "stretch"
   },
   text: {
     color: "#303030",
@@ -46,61 +47,106 @@ const styles = StyleSheet.create({
   }
 });
 
-export default ({ navigation }) => (
-  <ScrollView style={styles.scrollview}>
-  <View style={styles.container}>
+class MainMenu extends React.Component {
+  state = {
+    visible: false
+  };
+  _openMenu = () => this.setState({ visible: true });
 
-    <StatusBar barStyle="light-content" />
-    <Text style={styles.text}>Main Menu</Text>
-    <TouchableOpacity
-      onPress={() => navigation.navigate("ASP")}
-      style={styles.button}
-    >
-      <Text style={styles.buttonText}>ASP</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      onPress={() => navigation.navigate("CommunityPost")}
-      style={styles.button}
-    >
-      <Text style={styles.buttonText}>CommunityPost</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      onPress={() => navigation.navigate("CommunitySearch")}
-      style={styles.button}
-    >
-      <Text style={styles.buttonText}>CommunitySearch</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      onPress={() => navigation.navigate("CreateAccount")}
-      style={styles.button}
-    >
-      <Text style={styles.buttonText}>CreateAccount</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      onPress={() => navigation.navigate("FoodInfo")}
-      style={styles.button}
-    >
-      <Text style={styles.buttonText}>FoodInfo</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      onPress={() => navigation.navigate("ScanItem")}
-      style={styles.button}
-    >
-      <Text style={styles.buttonText}>ScanItem</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      onPress={() => navigation.navigate("UserLogin")}
-      style={styles.button}
-    >
-      <Text style={styles.buttonText}>UserLogin</Text>
-    </TouchableOpacity>
-    <TouchableOpacity
-      onPress={() => navigation.navigate("UserProfile")}
-      style={styles.button}
-    >
-      <Text style={styles.buttonText}>UserProfile</Text>
-    </TouchableOpacity>
-    </View>
+  _closeMenu = () => this.setState({ visible: false });
 
-    </ScrollView>
-);
+  render() {
+    const { navigate } = this.props.navigation;
+
+    return (
+      <ScrollView style={styles.scrollview}>
+        <Provider>
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "flex-end",
+              backgroundColor: "#fff"
+            }}
+          >
+            <Menu
+              visible={this.state.visible}
+              onDismiss={this._closeMenu}
+              anchor={<Button onPress={this._openMenu}>Menu</Button>}
+            >
+              <Menu.Item
+                onPress={() => {
+                  return navigate("MainMenu");
+                }}
+                title="Main Menu"
+              />
+              <Menu.Item
+                onPress={() => {
+                  return navigate("ASP");
+                }}
+                title="ASP"
+              />
+              <Menu.Item
+                onPress={() => {
+                  return navigate("CommunitySearch");
+                }}
+                title="Community"
+              />
+              <Menu.Item
+                onPress={() => {
+                  return navigate("ScanItem");
+                }}
+                title="Scan Item"
+              />
+              <Menu.Item
+                onPress={() => {
+                  return navigate("UserProfile");
+                }}
+                title="Profile"
+              />
+              <Menu.Item
+                onPress={() => {
+                  return navigate("UserLogin");
+                }}
+                title="Sign Out"
+              />
+            </Menu>
+          </View>
+          <View style={styles.container}>
+            <StatusBar barStyle="light-content" />
+            <Text
+              style={{
+                color: "#3bde26",
+                fontWeight: "bold",
+                textAlign: "center",
+                fontSize: 60,
+                paddingBottom: 30,
+                fontFamily: "HelveticaNeue-Italic"
+              }}
+            >
+              Revel
+            </Text>
+            <TouchableOpacity
+              onPress={() => navigate("FoodInfo")}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>FoodInfo</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigate("ScanItem")}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>ScanItem</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigate("UserProfile")}
+              style={styles.button}
+            >
+              <Text style={styles.buttonText}>UserProfile</Text>
+            </TouchableOpacity>
+          </View>
+        </Provider>
+      </ScrollView>
+    );
+  }
+}
+export default MainMenu;
